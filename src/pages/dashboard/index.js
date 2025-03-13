4428920f4e4e4184018643f8db5679f8e831d82f
gaurav sandhu <gaurav.sandhu@codebrewinnovations.com>
2025-03-13 18:05:30 +0530

    update

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Button } from "react-bootstrap";
+
+function Dashboard(){
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Button variant="primary">Primary</Button>
+        </div>
+    )
+}
+
+export default Dashboard;
